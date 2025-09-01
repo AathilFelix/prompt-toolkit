df3aa1831495b13fea440a9ff005cf7aa201da0d
Aathil Felix <aathil205@gmail.com>
2025-09-01 22:41:23 +0530

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ PORT=${1:-8000}
 
 lsof -ti:$PORT | xargs kill -9 2>/dev/null || true
 
-git pull origin main
+git pull origin master
 
 # Create and activate virtual environment
 python3 -m venv venv
